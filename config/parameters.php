7f77fe0a0e693604c3073864eeea12a3d4e5f566
Adriana <capataadriana@yahoo.com>
2020-06-02 12:49:24 +0300

    parameters file

--- config/parameters.php
+++ config/parameters.php
@@ -2,8 +2,24 @@
 
 return $parameters = [
     'db' => [
-        'hostname' => 'mysql:host=localhost;dbname=blogger',
-        'username' => 'root',
-        'password' => null,
-        ]
+        'hostname' => 'mysql:host=localhost;dbname=your_dbname',
+        'username' => 'your_username',
+        'password' => 'your_db_password',
+        ],
+    'swift_mailer' => [
+        'host' => 'smtp_mail',
+        'port' => 'some_port',
+        'encryption' => 'some_encryption',
+        'sender_address' => '',
+        'delivery_address' => [''],
+        'password' => 'password'
+    ],
+    'memcached' => [
+        'host' => '',
+        'port' => 'some_port'
+    ],
+    'logger' => [
+        'path' => 'path_to_log_file',
+        'type' => 'file_or_db',
+    ]
 ];
